0febfda52db8a023f1d0f0dd75e8bf3f2ab36392
Nils Ponsard <nilsponsard@gmail.com>
2022-07-15 10:22:28 +0200

    feat: initialize client
    Signed-off-by: Nils Ponsard <nilsponsard@gmail.com>

--- kudoctl/src/main.rs
+++ kudoctl/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use log::trace;
+use reqwest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    trace!("Logger initialized");
+
+    let client = reqwest::Client::new();
+
+    trace!("reqwest Client initialized");
+    Ok(())
 }
