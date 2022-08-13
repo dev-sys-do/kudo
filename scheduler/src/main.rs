3199c85ed06865419a2b21b1326f755d56ff1134
iverly <github@iverly.net>
2022-08-13 17:09:28 +0200

    feat(scheduler): add manager struct, node struct and logger
    Signed-off-by: iverly <github@iverly.net>

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -1,3 +1,14 @@
+use log::{info, debug};
+use scheduler::manager::Manager;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    info!("starting up");
+
+    let scheduler = Manager::new();
+    debug!("initialized manager struct with {:?}", scheduler);
+    scheduler.run();
+
+    info!("shutting down");
 }
