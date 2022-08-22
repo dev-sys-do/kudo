ea6080542210e545d18aad4faa075b870300b92f
iverly <github@iverly.net>
2022-08-22 13:18:21 +0200

    feat(scheduler): add create grpc server method
    Signed-off-by: iverly <github@iverly.net>

--- scheduler/src/manager.rs
+++ scheduler/src/manager.rs
@@ -1,53 +1,76 @@
-use proto::scheduler::Instance;
-use log::{info, debug};
+use log::{debug, info};
+use proto::scheduler::{node_service_server::NodeServiceServer, Instance};
 use tokio::task::JoinHandle;
+use tonic::transport::Server;
 
 use crate::{storage::Storage, Node};
 
 #[derive(Debug)]
 pub struct Manager {
     instances: Storage<Instance>,
-    nodes: Storage<Node>
+    nodes: Storage<Node>,
 }
 
 impl Manager {
     /// `new` creates a new `Manager` struct with two empty `Storage` structs
-    /// 
+    ///
     /// Returns:
-    /// 
+    ///
     /// A new Manager struct
     pub fn new() -> Self {
         Manager {
             instances: Storage::new(),
-            nodes: Storage::new()
+            nodes: Storage::new(),
         }
     }
-    
+
     /// This function returns a reference to the instances storage.
-    /// 
+    ///
     /// Returns:
-    /// 
+    ///
     /// A reference to the instances storage.
     pub fn get_instances_storage(&self) -> &Storage<Instance> {
         &self.instances
     }
 
     ///This function returns a reference to the nodes storage.
-    /// 
+    ///
     /// Returns:
-    /// 
+    ///
     /// A reference to the nodes storage.
     pub fn get_nodes_storage(&self) -> &Storage<Node> {
         &self.nodes
     }
 
-    /// This function runs the scheduler.
-    /// 
+    /// This function creates a gRPC server that listens on port 50051 and registers the
+    /// `NodeServiceServer` with the server
+    ///
     /// Returns:
-    /// 
+    ///
+    /// A JoinHandle<()>
+    fn create_grpc_server(&self) -> JoinHandle<()> {
+        info!("creating grpc server ...");
+
+        let addr = "127.0.0.1:50051".parse().unwrap();
+
+        tokio::spawn(async move {
+            info!("started grpc server at {}", addr);
+
+            Server::builder().serve(addr).await.unwrap();
+        })
+    }
+
+    /// This function runs the scheduler.
+    ///
+    /// Returns:
+    ///
     /// A vector of JoinHandles.
     pub fn run(&self) -> Result<Vec<JoinHandle<()>>, Box<dyn std::error::Error>> {
         let mut handlers = vec![];
+
+        // create listeners and serve the grpc server
+        handlers.push(self.create_grpc_server());
+
         info!("scheduler running and ready to receive incoming requests ...");
         Ok(handlers)
     }
