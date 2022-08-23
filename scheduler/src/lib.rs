992d4d5fbd157f24170bd36fdd3cf5c0abffb068
iverly <github@iverly.net>
2022-08-23 09:16:10 +0200

    feat(scheduler): add event loop architecture
    We have chosen an event loop architecture approach for inter-thread communication. Once we received a gRPC call from one of the services, it will be sent to the Manager thread through an mpsc channel, procced and the result are sent through a oneshoot channel.
    
    We have chosen this architecture for the following reasons :
    - Handle multiple concurrent events processing
    - Avoid launching a new thread for each event received
    - Examples of mspc channels seen with @sameo
    
    Signed-off-by: iverly <github@iverly.net>

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -1,9 +1,51 @@
+use proto::scheduler::{
+    Instance, InstanceStatus, NodeRegisterRequest, NodeRegisterResponse, NodeStatus,
+    NodeUnregisterRequest, NodeUnregisterResponse,
+};
+use tokio::sync::{mpsc, oneshot};
+use tonic::Response;
+
 pub mod instance_listener;
 pub mod manager;
 pub mod node_listener;
 pub mod storage;
 
 #[derive(Debug)]
+#[allow(dead_code)]
 pub struct Node {
     id: String,
+}
+
+pub type NodeIdentifier = String;
+
+#[derive(Debug)]
+pub enum Event {
+    // Instance events
+    InstanceCreate(
+        Instance,
+        mpsc::Sender<Result<InstanceStatus, tonic::Status>>,
+    ),
+    InstanceStart(
+        NodeIdentifier,
+        oneshot::Sender<Result<Response<()>, tonic::Status>>,
+    ),
+    InstanceStop(
+        NodeIdentifier,
+        oneshot::Sender<Result<Response<()>, tonic::Status>>,
+    ),
+    InstanceDestroy(
+        NodeIdentifier,
+        oneshot::Sender<Result<Response<()>, tonic::Status>>,
+    ),
+
+    // Node events
+    NodeRegister(
+        NodeRegisterRequest,
+        oneshot::Sender<Result<Response<NodeRegisterResponse>, tonic::Status>>,
+    ),
+    NodeUnregister(
+        NodeUnregisterRequest,
+        oneshot::Sender<Result<Response<NodeUnregisterResponse>, tonic::Status>>,
+    ),
+    NodeStatus(NodeStatus, mpsc::Sender<Result<(), tonic::Status>>),
 }
