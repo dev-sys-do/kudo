72526142a7ffae32c049bd0242334b7e3fe7c651
Alexandre Gomez <gomez.a.corneille@gmail.com>
2022-08-23 18:07:31 +0200

    feat: implement config on controller
    Implemented configuration through `controller.conf` file
    
    Signed-off-by: Alexandre Gomez <gomez.a.corneille@gmail.com>

---
+++ controller/src/config.rs
@@ -0,0 +1,41 @@
+use serde::{Deserialize, Serialize};
+use std::net::{Ipv4Addr, SocketAddr};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct KudoControllerConfig {
+    pub internal_api: InternalAPIConfig,
+    pub external_api: ExternalAPIConfig,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct InternalAPIConfig {
+    pub grpc_server_addr: SocketAddr,
+    pub grpc_server_num_workers: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ExternalAPIConfig {
+    pub http_server_addr: SocketAddr,
+    pub http_server_num_workers: usize,
+}
+
+impl Default for KudoControllerConfig {
+    fn default() -> Self {
+        KudoControllerConfig {
+            internal_api: InternalAPIConfig {
+                grpc_server_addr: SocketAddr::new(
+                    std::net::IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                    50051,
+                ),
+                grpc_server_num_workers: 1,
+            },
+            external_api: ExternalAPIConfig {
+                http_server_addr: SocketAddr::new(
+                    std::net::IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                    3000,
+                ),
+                http_server_num_workers: 1,
+            },
+        }
+    }
+}
--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,24 +1,30 @@
 use controller_lib::external_api;
 use controller_lib::internal_api;
 
+use std::error::Error;
+
+mod config;
+
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let grpc_server_addr: String = "0.0.0.0:50051".to_string();
-    let grpc_server_num_workers: usize = 1;
-
-    let http_server_addr: String = "0.0.0.0:3000".to_string();
-    let http_server_num_workers: usize = 1;
-
+async fn main() -> Result<(), Box<dyn Error>> {
     // Init Logger
     env_logger::init();
 
+    let config: config::KudoControllerConfig = confy::load_path("controller.conf")?;
+
     // gRPC Server
-    internal_api::interface::InternalAPIInterface::new(grpc_server_addr, grpc_server_num_workers)
-        .await;
+    internal_api::interface::InternalAPIInterface::new(
+        config.internal_api.grpc_server_addr,
+        config.internal_api.grpc_server_num_workers,
+    )
+    .await;
 
     // HTTP Server
-    external_api::interface::ExternalAPIInterface::new(http_server_addr, http_server_num_workers)
-        .await;
+    external_api::interface::ExternalAPIInterface::new(
+        config.external_api.http_server_addr,
+        config.external_api.http_server_num_workers,
+    )
+    .await;
 
     Ok(())
 }
