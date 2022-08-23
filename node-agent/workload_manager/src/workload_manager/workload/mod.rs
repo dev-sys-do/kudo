7ae1915827669b53d916f8eff67d6d49edd823bc
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2022-08-23 16:28:35 +0200

    Abstract workload creation
    Create workload based on types in a transparent way
    
    Signed-off-by: Alexis Langlet <alexis.langlet@etu.umontpellier.fr>

--- node-agent/workload_manager/src/workload_manager/workload/mod.rs
+++ node-agent/workload_manager/src/workload_manager/workload/mod.rs
@@ -1,1 +1,12 @@
+use anyhow::Result;
+use proto::agent::{Instance, Type};
+use workload_trait::Workload;
+
+mod container;
 pub mod workload_trait;
+
+pub async fn create(instance: Instance) -> Result<impl Workload> {
+    match instance.r#type() {
+        Type::Container => container::Container::new(instance).await,
+    }
+}
