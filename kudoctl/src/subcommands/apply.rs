18db3dc0e12100e8f07531ca357dda205b1a71da
Nils Ponsard <nilsponsard@gmail.com>
2022-08-23 15:50:21 +0200

    feat(kudoctl): read from stdin
    Signed-off-by: Nils Ponsard <nilsponsard@gmail.com>

--- kudoctl/src/subcommands/apply.rs
+++ kudoctl/src/subcommands/apply.rs
@@ -1,9 +1,11 @@
+use std::io::{self, Read};
+
 use crate::{
     client::{self, request::Client},
     config,
     resource::Resource,
 };
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use clap::Args;
 use log::{debug, info};
 
@@ -21,20 +23,31 @@ pub struct Apply {
 pub async fn execute(args: Apply, conf: &config::Config) -> Result<()> {
     let client = Client::new(conf).context("Error creating client")?;
 
-    // read the yaml file
-    if args.file.is_none() {
-        // Next : read the yaml file from stdin
-        return Err(anyhow::Error::msg("No file specified"));
-    }
+    // read the yaml file if -f is used
+    let yaml = if let Some(file) = args.file {
+        debug!("Reading file {}", file);
 
-    let file = args.file.unwrap();
-    debug!("Reading file {}", file);
+        std::fs::read_to_string(file.clone())
+            .with_context(|| format!("Error reading file {}", file))?
+    } else {
+        // Read the yaml file from stdin otherwise
 
-    let yaml = std::fs::read_to_string(file.clone())
-        .with_context(|| format!("Error reading file {}", file))?;
+        let mut buffer = String::new();
+        let mut stdin = io::stdin();
+
+        stdin
+            .read_to_string(&mut buffer)
+            .context("Error reading stdin")?;
+
+        if buffer.is_empty() {
+            bail!("No yaml file provided, please use the -f option or pass the yaml file as stdin");
+        }
+        buffer
+    };
+
     // parse the yaml file
     let resource_data: Resource =
-        serde_yaml::from_str(&yaml).with_context(|| format!("Error parsing file {}", file))?;
+        serde_yaml::from_str(&yaml).context("Error parsing file resource")?;
 
     if args.no_update {
         // TODO: check if the workload already exists
