3c33b844527ee6647f69f4f395d0c4abe0f8e586
Alexandre Gomez <gomez.a.corneille@gmail.com>
2022-08-23 18:07:31 +0200

    feat: implement config on controller
    Implemented configuration through `controller.conf` file
    
    Signed-off-by: Alexandre Gomez <gomez.a.corneille@gmail.com>

--- controller/lib/src/internal_api/interface.rs
+++ controller/lib/src/internal_api/interface.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+
 use super::node::controller::NodeController;
 use log::info;
 use proto::controller::node_service_server::NodeServiceServer;
@@ -6,18 +8,17 @@ use tonic::transport::Server;
 pub struct InternalAPIInterface {}
 
 impl InternalAPIInterface {
-    pub async fn new(address: String, num_workers: usize) -> Self {
+    pub async fn new(address: SocketAddr, num_workers: usize) -> Self {
         info!(
             "Starting {} gRPC worker(s) listening on {}",
             num_workers, address
         );
 
         for _ in 1..num_workers {
-            let address = address.clone();
             tokio::spawn(async move {
                 Server::builder()
                     .add_service(NodeServiceServer::new(NodeController::default()))
-                    .serve(address.parse().unwrap())
+                    .serve(address)
                     .await
                     .unwrap();
             });
