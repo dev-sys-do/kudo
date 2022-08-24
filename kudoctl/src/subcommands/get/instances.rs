ea6a85987143f1a90750b85ecef3e93af1cd7824
Nils Ponsard <nilsponsard@gmail.com>
2022-08-24 11:19:15 +0200

    feat(kudoctl): get instances command
    Signed-off-by: Nils Ponsard <nilsponsard@gmail.com>

---
+++ kudoctl/src/subcommands/get/instances.rs
@@ -0,0 +1,33 @@
+use super::output::{self, OutputFormat};
+use crate::{
+    client::{self, instance::GetInstancesResponse, request::Client},
+    config,
+};
+use anyhow::{Context, Result};
+use std::fmt::Display;
+
+/// get instances subcommand execution
+/// Does the request, then formats the output.
+pub async fn execute(
+    conf: &config::Config,
+    format: OutputFormat,
+    show_header: bool,
+) -> Result<String> {
+    let client = Client::new(conf).context("Error creating client")?;
+    let mut result = client::instance::list(&client).await?;
+    result.show_header = show_header;
+    output::format_output(result, format)
+}
+
+impl Display for GetInstancesResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.show_header {
+            writeln!(f, "ID\tSTATUS")?;
+        }
+
+        for inst in &self.instances {
+            writeln!(f, "{}\t{}\n", inst.id, inst.status)?;
+        }
+        Ok(())
+    }
+}
