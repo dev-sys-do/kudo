e06b6163c4a132b56af084a00110063d4fabc6df
iverly <github@iverly.net>
2022-08-28 12:41:06 +0200

    feat(scheduler): add configuration file
    Signed-off-by: iverly <github@iverly.net>

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -2,13 +2,29 @@ use proto::scheduler::{
     Instance, InstanceStatus, NodeRegisterRequest, NodeRegisterResponse, NodeStatus,
     NodeUnregisterRequest, NodeUnregisterResponse,
 };
+use thiserror::Error;
 use tokio::sync::{mpsc, oneshot};
 use tonic::Response;
 
+pub mod config;
 pub mod instance_listener;
 pub mod manager;
 pub mod node_listener;
 pub mod storage;
+
+#[derive(Error, Debug)]
+pub enum SchedulerError {
+    #[error("unable to read the configuration file's path")]
+    ConfigPathReadError(#[from] std::io::Error),
+    #[error("unable to read the configuration file")]
+    ConfigReadError(#[from] confy::ConfyError),
+    #[error("invalid grpc address in configuration file")]
+    InvalidGrpcAddress,
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
+    #[error("unknown scheduler error")]
+    Unknown,
+}
 
 #[derive(Debug)]
 #[allow(dead_code)]
--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -1,12 +1,23 @@
+use std::env;
+
 use log::{debug, info};
-use scheduler::manager::Manager;
+use scheduler::{config::Config, manager::Manager, SchedulerError};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     info!("starting up");
 
-    let manager = Manager::new();
+    info!("loading config");
+    let mut dir = env::current_dir().map_err(SchedulerError::ConfigPathReadError)?; // get executable path
+    dir.push("scheduler.conf"); // add config file name
+
+    // load config from path
+    let config: Config =
+        confy::load_path(dir.as_path()).map_err(SchedulerError::ConfigReadError)?;
+    debug!("config: {:?}", config);
+
+    let manager = Manager::new(config);
     debug!("initialized manager struct with data : {:?}", manager);
 
     manager.run().await?;
