2229e617c91c39b5c1a83250f4204ed6cb90c7e9
esteban <esteban.baron@etu.umontpellier.fr>
2022-08-29 19:10:43 +0200

    feat: implement grpc server in node agent
    In this commit, you can find the implementation of the node agent grpc server.
    This server handle scheduler request by using the workload_manager library (node agent internal crate).
    
    Signed-off-by: esteban <esteban.baron@etu.umontpellier.fr>

--- node-agent/src/main.rs
+++ node-agent/src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
 use std::net::Ipv4Addr;
 use std::str::FromStr;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::thread::sleep;
 use std::time::Duration;
 
 use cidr::Ipv4Inet;
 use log::{debug, info, trace};
 use network::node::request::SetupNodeRequest;
+use tokio::sync::mpsc::channel;
+use tokio::sync::Mutex;
 use tokio::time;
+use tokio_stream::wrappers::ReceiverStream;
 use tonic::{transport::Server, Request, Response, Status};
 use uuid::Uuid;
 
@@ -17,13 +20,104 @@ mod config;
 use config::{GrpcServerConfig, NodeAgentConfig};
 use network::node::setup_node;
 use node_manager::NodeSystem;
+use workload_manager::workload_manager::WorkloadManager;
 
+use proto::agent::{
+    instance_service_server::InstanceService, instance_service_server::InstanceServiceServer,
+    Instance, InstanceStatus, SignalInstruction,
+};
 use proto::scheduler::{
     node_service_client::NodeServiceClient, NodeRegisterRequest, NodeRegisterResponse, NodeStatus,
     Resource, ResourceSummary, Status as SchedulerStatus,
 };
 
 const NUMBER_OF_CONNECTION_ATTEMPTS: u16 = 10;
+
+///
+/// This Struct implement the Instance service from Node Agent proto file
+///
+pub struct InstanceServiceController {
+    workload_manager: Arc<Mutex<WorkloadManager>>,
+}
+
+impl InstanceServiceController {
+    pub fn new(node_id: String) -> Self {
+        Self {
+            workload_manager: Arc::new(Mutex::new(WorkloadManager::new(node_id))),
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl InstanceService for InstanceServiceController {
+    type createStream = ReceiverStream<Result<InstanceStatus, Status>>;
+
+    async fn create(
+        &self,
+        request: Request<Instance>,
+    ) -> Result<Response<Self::createStream>, Status> {
+        let instance = request.into_inner();
+        let channel = channel(1024);
+
+        // call workload_manager create function in an other thread
+        let workload_manager = self.workload_manager.clone();
+
+        tokio::spawn(async move {
+            workload_manager
+                .clone()
+                .lock()
+                .await
+                .create(instance, channel.0.clone())
+                .await
+                .map_err(|err| Status::internal(format!("Workload creation error: {}", err)))
+                .ok();
+        });
+
+        // send receiver to scheduler
+        Ok(Response::new(ReceiverStream::new(channel.1)))
+    }
+
+    async fn signal(&self, request: Request<SignalInstruction>) -> Result<Response<()>, Status> {
+        let signal_instruction = request.into_inner();
+
+        // call workload_manager signal function in an other thread
+        let workload_manager = self.workload_manager.clone();
+
+        tokio::spawn(async move {
+            workload_manager
+                .clone()
+                .lock()
+                .await
+                .signal(signal_instruction)
+                .await
+                .map_err(|_| Status::internal("Cannot send signal to the workload"))
+                .ok();
+        });
+
+        Ok(Response::new(()))
+    }
+}
+
+///
+/// This function starts the grpc server of the Node Agent.
+/// The server listens and responds to requests from the Scheduler.
+/// The default port is 50053.
+///
+fn create_grpc_server(config: GrpcServerConfig, node_id: String) -> tokio::task::JoinHandle<()> {
+    let addr = format!("{}:{}", config.host, config.port).parse().unwrap();
+    let instance_service_controller = InstanceServiceController::new(node_id);
+
+    info!("Node Agent server listening on {}", addr);
+
+    tokio::spawn(async move {
+        Server::builder()
+            .add_service(InstanceServiceServer::new(instance_service_controller))
+            .serve(addr)
+            .await
+            .unwrap()
+    })
+}
+
 ///
 /// This function allows you to connect to the scheduler's grpc server.
 ///
@@ -48,25 +142,24 @@ async fn register_to_scheduler(
 ///
 /// This function allows you to send node status to the scheduler's grpc server.
 ///
-async fn send_node_satus_to_scheduler(
+async fn send_node_status_to_scheduler(
     client: &mut NodeServiceClient<tonic::transport::Channel>,
-    node_system_arc: Arc<std::sync::Mutex<NodeSystem>>,
+    node_system_arc: Arc<Mutex<NodeSystem>>,
     node_id: String,
 ) -> Option<tonic::Response<()>> {
     let node_status_stream = async_stream::stream! {
         let mut interval = time::interval(Duration::from_secs(1));
 
+        let cpu_limit = node_system_arc.lock().await.total_cpu();
+        let memory_limit = node_system_arc.lock().await.total_memory();
+        let disk_limit = node_system_arc.lock().await.total_disk();
+
         loop {
             interval.tick().await;
 
-            let cpu_limit = node_system_arc.lock().unwrap().total_cpu();
-            let cpu_usage = node_system_arc.lock().unwrap().used_cpu();
-
-            let memory_limit = node_system_arc.lock().unwrap().total_memory();
-            let memory_usage = node_system_arc.lock().unwrap().used_memory();
-
-            let disk_limit = node_system_arc.lock().unwrap().total_disk();
-            let disk_usage = node_system_arc.lock().unwrap().used_disk();
+            let cpu_usage = node_system_arc.lock().await.used_cpu();
+            let memory_usage = node_system_arc.lock().await.used_memory();
+            let disk_usage = node_system_arc.lock().await.used_disk();
 
             let node_status = NodeStatus {
                 id: node_id.clone(),
@@ -128,11 +221,11 @@ fn create_grpc_client(config: GrpcServerConfig, node_id: String) -> tokio::task:
         // Registration with the Scheduler
 
         let certificate = node_id.clone();
-        let mut registration = register_to_scheduler(&mut client, certificate).await;
+        let mut registration = register_to_scheduler(&mut client, certificate.clone()).await;
 
-        // setup node
+        // setup node network
 
-        let node_ip = registration.unwrap().into_inner().id;
+        let node_ip = registration.unwrap().into_inner().ip;
         let node_ip_addr = Ipv4Addr::from_str(&node_ip).unwrap();
         let node_ip_cidr = Ipv4Inet::new(node_ip_addr, 24).unwrap();
 
@@ -145,7 +238,7 @@ fn create_grpc_client(config: GrpcServerConfig, node_id: String) -> tokio::task:
                 sleep(Duration::from_secs(1));
 
                 debug!("Registration to the Scheduler failed, retrying...");
-                registration = register_to_scheduler(&mut client, certificate).await;
+                registration = register_to_scheduler(&mut client, certificate.clone()).await;
 
                 attempts += 1;
             } else {
@@ -160,19 +253,23 @@ fn create_grpc_client(config: GrpcServerConfig, node_id: String) -> tokio::task:
         let node_system = NodeSystem::new();
         let arc_node_system = Arc::new(Mutex::new(node_system));
 
-        let mut send_node_status_to_scheduler =
-            send_node_satus_to_scheduler(&mut client, Arc::clone(&arc_node_system), node_id).await;
+        let mut send_node_resources_to_scheduler = send_node_status_to_scheduler(
+            &mut client,
+            Arc::clone(&arc_node_system),
+            node_id.clone(),
+        )
+        .await;
 
         attempts = 0;
-        while send_node_status_to_scheduler.is_none() {
+        while send_node_resources_to_scheduler.is_none() {
             if attempts <= NUMBER_OF_CONNECTION_ATTEMPTS {
                 sleep(Duration::from_secs(1));
 
                 debug!("Sending node status to the Scheduler failed, retrying...");
-                send_node_status_to_scheduler = send_node_satus_to_scheduler(
+                send_node_resources_to_scheduler = send_node_status_to_scheduler(
                     &mut client,
                     Arc::clone(&arc_node_system),
-                    node_id,
+                    node_id.clone(),
                 )
                 .await;
 
@@ -183,6 +280,7 @@ fn create_grpc_client(config: GrpcServerConfig, node_id: String) -> tokio::task:
         }
     })
 }
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
@@ -205,8 +303,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // start grpc server and client
     let client_handler = create_grpc_client(config.client, node_id.clone());
+    let server_handler = create_grpc_server(config.server, node_id.clone());
 
     client_handler.await?;
+    server_handler.await?;
 
     info!("Shutting down node agent");
 
