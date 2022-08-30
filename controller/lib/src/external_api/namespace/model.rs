60677aaeb62e1404f7faa9d6145e6c8ba8e93bd8
Maxime <maxime.pizzolitto.82@gmail.com>
2022-08-30 10:02:27 +0200

    feat : create namespace service
    Namespace service implements all the logic to manage Namespace.
    Workload service implements EtcdService which is the interface to interact with etcd.
    
    Things to know : 
    
    - Namespace service require etcd address when initialized
    
    - Etcd save in key => value format.
    The key is the word "namespace" concatenated with the namespace name
    
    Signed-off-by: Maxime <maxime.pizzolitto.82@gmail.com>

---
+++ controller/lib/src/external_api/namespace/model.rs
@@ -0,0 +1,72 @@
+use actix_web::HttpResponse;
+use serde::{Deserialize, Serialize};
+
+pub enum NamespaceError {
+    NotFound,
+    Etcd(String),
+    NameAlreadyExists(String),
+    JsonToNamespace(String),
+    NamespaceToJson(String),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Metadata {}
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Namespace {
+    pub id: String,
+    pub name: String,
+    pub metadata: Metadata,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct NamespaceDTO {
+    pub name: String,
+}
+
+impl NamespaceError {
+    pub fn to_http(&self) -> HttpResponse {
+        match self {
+            NamespaceError::NotFound => HttpResponse::NotFound().body("Namespace not found"),
+            NamespaceError::Etcd(err) => {
+                HttpResponse::InternalServerError().body(format!("Etcd error: {} ", err))
+            }
+            NamespaceError::NameAlreadyExists(name) => HttpResponse::Conflict()
+                .body(format!("Namespace with name {} already exists", name)),
+            NamespaceError::JsonToNamespace(err) => HttpResponse::InternalServerError().body(
+                format!("Error while converting JSON string to Namespace : {}", err),
+            ),
+            NamespaceError::NamespaceToJson(err) => HttpResponse::InternalServerError().body(
+                format!("Error while converting the Namespace to JSON: {}", err),
+            ),
+        }
+    }
+}
+impl Namespace {
+    pub fn to_http(&self) -> HttpResponse {
+        match serde_json::to_string(&self) {
+            Ok(json) => HttpResponse::Ok().body(json),
+            Err(err) => HttpResponse::InternalServerError().body(format!(
+                "Error while converting the Namespace to JSON: {}",
+                err
+            )),
+        }
+    }
+}
+#[derive(Deserialize, Serialize)]
+pub struct NamespaceVector {
+    pub namespaces: Vec<Namespace>,
+}
+impl NamespaceVector {
+    pub fn new(namespaces: Vec<Namespace>) -> NamespaceVector {
+        NamespaceVector { namespaces }
+    }
+    pub fn to_http(&self) -> HttpResponse {
+        match serde_json::to_string(&self.namespaces) {
+            Ok(json) => HttpResponse::Ok().body(json),
+            Err(err) => HttpResponse::InternalServerError().body(format!(
+                "Error while converting the Namespace to JSON: {}",
+                err
+            )),
+        }
+    }
+}
