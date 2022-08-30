d2e91d1ca7c0130cef24668e386592041893a032
iverly <github@iverly.net>
2022-08-30 12:13:08 +0200

    feat(scheduler): add NodeStatus handler
    Signed-off-by: iverly <github@iverly.net>

--- scheduler/src/event/handlers/mod.rs
+++ scheduler/src/event/handlers/mod.rs
@@ -2,4 +2,5 @@ pub mod instance_create;
 pub mod instance_destroy;
 pub mod instance_stop;
 pub mod node_register;
+pub mod node_status;
 pub mod node_unregister;
---
+++ scheduler/src/event/handlers/node_status.rs
@@ -0,0 +1,43 @@
+use std::sync::Arc;
+
+use crate::orchestrator::Orchestrator;
+use anyhow::Result;
+use proto::scheduler::NodeStatus;
+use tokio::sync::{mpsc, Mutex};
+
+pub struct NodeStatusHandler {}
+
+impl NodeStatusHandler {
+    pub async fn handle(
+        orchestrator: Arc<Mutex<Orchestrator>>,
+        status: NodeStatus,
+        tx: mpsc::Sender<Result<(), tonic::Status>>,
+    ) {
+        match orchestrator
+            .lock()
+            .await
+            .update_node_status(status.id.clone(), status.clone())
+            .await
+        {
+            Ok(_) => {
+                log::debug!("successfully updated node status : {:?}", status.id);
+
+                tx.send(Ok(())).await.unwrap();
+            }
+            Err(err) => {
+                log::error!(
+                    "error while updating node status : {:?} ({:?})",
+                    status.id,
+                    err
+                );
+
+                tx.send(Err(tonic::Status::internal(format!(
+                    "Error thrown by the orchestrator: {:?}",
+                    err
+                ))))
+                .await
+                .unwrap();
+            }
+        };
+    }
+}
